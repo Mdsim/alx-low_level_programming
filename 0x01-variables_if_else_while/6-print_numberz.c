88853a1331d889dce8dd0c35c4328ebffee2b4b0
mdsim <mdsimultaneous1@gmail.com>
2023-10-07 23:15:13 +0100

    36

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Description: Numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
