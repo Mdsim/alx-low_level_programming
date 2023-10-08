2979a5d170c709f91bebbe0a8e3b8e2439b9e393
mdsim <mdsimultaneous1@gmail.com>
2023-10-08 07:16:16 +0100

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Description: Printing Base 16
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; 1 < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 93; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
